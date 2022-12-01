3edc51fa72c144dc633526bad5b11956bedc4380
Mocren <Kl.Artur@web.de>
2022-12-01 16:28:20 +0100

    Finished PC turn and started with players turn

--- src/missionWithoutNorbert/Main.java
+++ src/missionWithoutNorbert/Main.java
@@ -1,10 +1,10 @@
 package missionWithoutNorbert;
 
-import java.util.Scanner;
+import de.hsrm.mi.prog.util.StaticScanner;
 
 /*
 Erstes Stegreifprojekt "MissionWithoutNorbert"
-Implementiert von Mykhailo Fakliier, Fouad und Artur Konkel	
+Implementiert von Mykhailo Fakliier, Fouad Ahsayni und Artur Konkel	
 	
 */
 
@@ -12,20 +12,27 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner scan = new Scanner(System.in);
 
-		final int MAX_MINION_QUEUE = 10;
+		final int MAX_MINION_QUEUE = 11;
 		final int MAX_SELECTED_MINIONS_ALLOWED = 3;
 
 		boolean playersTurn;
 		int norbertsPosition;
 		String minionQueue;
 		int numOfChosenMinions = 0;
-		boolean selectedLeftSide;
+		boolean selectedLeftSide = true;
 		boolean turnPossible = false;
-
+		boolean inputCorrect = false;
+		
+		
 		int minionsLeftOfNorbert;
 		int minionsRightOfNorbert;
+		
+		int playerInputNumMinions;
+		char playerInputSide;
+
+		int teamPC = 0;
+		int teamPlayer = 0;
 
 		// Choose who will start
 		playersTurn = coinFlip();
@@ -44,25 +51,109 @@ public class Main {
 
 			if (playersTurn) {
 				// Players turn
+				System.out.println("Auf welcher Seite möchten Sie ihre Minions wählen? Geben Sie rechts(r) oder links(l) ein.");
+				while (!inputCorrect) {
+					playerInputSide = StaticScanner.nextChar();
+					
+					if(playerInputSide != 'r' || playerInputSide != 'l') {
+						System.out.println("Ihre Eingabe ist leider ungültig. Versuchen Sie es bitte erneut.");
+					} else {
+						if(playerInputSide == 'l') {
+							selectedLeftSide = true;
+						} else {
+							selectedLeftSide = false;
+						}
+					}
+				}
+				
+				System.out.println("Wieviele Minions möchten Sie wählen? Wählen Sie zwischen 1 und 3 Minions.");
+				
+				//Check if input are correct	
+				inputCorrect = false;
+				while (!inputCorrect) {
+					playerInputNumMinions = StaticScanner.nextInt();
+					
+					if(playerInputNumMinions < 1 || playerInputNumMinions > 3) {
+						System.out.println("Ihre Eingabe ist nicht korrekt. Bitte versuchen Sie es erneut");
+					} else {
+						numOfChosenMinions = playerInputNumMinions;
+						inputCorrect = true;
+					}
+				}
+				
+				
+				
 			} else {
 				// PC turn
 
 				while (!turnPossible) {
 					numOfChosenMinions = getRandomNumber(1, MAX_SELECTED_MINIONS_ALLOWED);
 					selectedLeftSide = coinFlip();
-
 					// Check if it's possible to select the chosen number of Minions on the chosen
 					// side
 					turnPossible = checkIfTurnPossible(minionsLeftOfNorbert, minionsRightOfNorbert, numOfChosenMinions,
 							selectedLeftSide);
-					System.out.println(turnPossible);
 				}
+				
+				if (selectedLeftSide) {
+					minionsLeftOfNorbert -= numOfChosenMinions;
+				} else {
+					minionsRightOfNorbert -= numOfChosenMinions;
+				}
+
+				teamPC = addSelectedMinionsToTeam(teamPC, numOfChosenMinions);
+				
+				minionQueue = adjustMinionQueue(selectedLeftSide, numOfChosenMinions, minionsLeftOfNorbert,
+						minionsRightOfNorbert);
+				
+				
+				System.out.println("PC has selected " + numOfChosenMinions + " Minions. ");
+				System.out.println(minionQueue);
+				
+				//Finished with Turn
+				playersTurn = !playersTurn;
+				
 
 			}
 
 			minionQueue = "__________";
 		}
 
+	}
+
+	public static String adjustMinionQueue(boolean selectedLeftSide, int numOfChosenMinions, int minionsLeftOfNorbert,
+			int minionsRightOfNorbert) {
+		String minionQueue = "";
+
+		if (selectedLeftSide) {
+			for (int i = 0; i < numOfChosenMinions; i++) {
+				minionQueue += "_";
+			}
+		}
+		for (int i = 0; i < minionsLeftOfNorbert; i++) {
+			minionQueue += "X";
+		}
+
+		// Add Norbert to Queue
+		minionQueue += "O";
+
+		for (int i = 0; i < minionsRightOfNorbert; i++) {
+			minionQueue += "X";
+		}
+		if (!selectedLeftSide)
+			for (int i = 0; i < numOfChosenMinions; i++) {
+				minionQueue += "_";
+
+			}
+
+		return minionQueue;
+
+	}
+
+	public static int addSelectedMinionsToTeam(int team, int numOfChosenMinions) {
+		team += numOfChosenMinions;
+
+		return team;
 	}
 
 	public static int determineMinionsLeftOfNorbert(int norbertsPosition) {
